68d54d5943d94e4088c3035e359a8d6334b0ccc4
Serine <syrineissaad2@gmail.com>
2022-03-05 11:20:39 +0100

    3nd test commit, v1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("commiting v1.2");
+        error to test
     }
 }
